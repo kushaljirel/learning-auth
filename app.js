4b67dba771388d30da86d3ec848724af8ab765d9
kushaljirel <kushaljirel12@gmail.com>
2024-05-18 12:30:20 +0545

    level03 password hashing

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 import dotenv from "dotenv";
 dotenv.config({
   path: "./.env.local",
@@ -6,29 +5,29 @@ dotenv.config({
 
 import express from "express";
 import bodyParser from "body-parser";
+import md5 from 'md5'
 
 import connectDB from "./connectDB.js";
 import { User } from "./model/models.js";
 import { Secret } from "./model/models.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Database connection
 await connectDB();
-
-const PORT = process.env.PORT || 3000;
-
-// console.log(typeof process.env.MONGO_URI)
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// home route
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+// login route
 app
   .route("/login")
   .get((req, res) => {
@@ -43,7 +42,7 @@ app
       if (!foundUser) {
         res.send("User not found!");
       } else {
-        if (foundUser.password === password) {
+        if (foundUser.password === md5(password)) {
           console.log("User Logged Successfully");
           res.redirect("/secrets");
         } else {
@@ -57,6 +56,7 @@ app
     }
   });
 
+// register route  
 app
   .route("/register")
   .get((req, res) => {
@@ -65,15 +65,15 @@ app
   .post(async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    // console.log(username, password)
+
     try {
       const user = new User({
         username: req.body.username,
-        password: req.body.password,
+        password: md5(req.body.password),
       });
       await user.save();
       console.log("User registered Successfully");
-      res.send("register success");
+      res.redirect("/secrets");
     } catch (error) {
       console.log("User failed to register!");
       console.log(error);
@@ -81,10 +81,12 @@ app
     }
   });
 
+// secret route  
 app.route("/secrets").get((req, res) => {
   res.render("secrets");
 });
 
+// submit secrets route
 app
   .route("/submit")
   .get((req, res) => {
@@ -97,14 +99,21 @@ app
       });
       userSecret.save();
       console.log("secret message saved");
-      res.redirect("submit");
+      res.redirect("/secrets");
     } catch (error) {
       console.log("couldnt save secret message");
       console.log(error);
-      res.redirect("submit");
+      res.redirect("/submit");
     }
   });
 
+// logout route
+app.route("/logout")
+.get((req, res)=>{
+    res.redirect("/")
+})
+
+// listening to port
 app.listen(PORT, () => {
   console.log(`APP is running in port ${PORT}`);
 });
--- model/models.js
+++ model/models.js
@@ -1,42 +1,31 @@
-import dotenv from "dotenv";
-dotenv.config({
-  path: "./.env.local",
-});
-import mongoose, { Schema } from 'mongoose'
-import encrypt from 'mongoose-encryption'
+import mongoose, { Schema } from "mongoose";
 
-
-
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-}, {timestamps: true})
+  },
+  { timestamps: true }
+);
 
-const secretSchema = new Schema({
+const secretSchema = new Schema(
+  {
     secret: {
-        type: String,
-        required: true
-    }
-}, {timestamps: true})
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const userKey = process.env.USER_KEY
-const secretKey = process.env.SECRET_KEY
+const User = mongoose.model("User", userSchema);
+const Secret = mongoose.model("Secret", secretSchema);
 
-console.log(userKey, secretKey)
-
-userSchema.plugin(encrypt, { secret: userKey, encryptedFields: ['password'] });
-secretSchema.plugin(encrypt, { secret: secretKey, encryptedFields: ['secret'] });
-
-
-const User = mongoose.model('User', userSchema);
-const Secret = mongoose.model('Secret', secretSchema);
-
-
-export { User, Secret }
+export { User, Secret };
