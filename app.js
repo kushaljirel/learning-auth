cd5dd8a11d6ccdf72caadff5c24761c9ad1f40af
kushaljirel <kushaljirel12@gmail.com>
2024-05-24 20:35:48 +0545

    Using Passportjs to Add Cookies and Sessions

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ dotenv.config({
 
 import express from "express";
 import bodyParser from "body-parser";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
 
 import connectDB from "./connectDB.js";
 import { User } from "./model/models.js";
@@ -22,11 +26,95 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// setup session middleware
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_URI }),
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      maxAge: 600000,
+    },
+  })
+);
+
+// initialize passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// configure local strategy for passport
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const foundUser = await User.findOne({ username });
+      if (!foundUser) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const isMatch = await bcrypt.compare(password, foundUser.password);
+      if (!isMatch) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, foundUser);
+    } catch (err) {
+      return done(err)
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 // home route
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+// register route
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post(async (req, res) => {
+    const { username, password } = req.body;
+
+    // for bcrypt password encryption
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    try {
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res.status(400).send("User already exists");
+      }
+
+      const newUser = new User({
+        username: username,
+        password: hash,
+      });
+      await newUser.save();
+
+      console.log("User registered Successfully");
+      res.status(200).redirect("/login");
+    } catch (error) {
+      console.log("User failed to register!");
+      console.log(error);
+      res.status(500).redirect("/register");
+    }
+  });
 
 // login route
 app
@@ -34,69 +122,30 @@ app
   .get((req, res) => {
     res.render("login");
   })
-  .post(async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  .post(
+    passport.authenticate("local", {
+      successRedirect: "/secrets",
+      failureRedirect: "/login",
+      failureFlash: false,
+    })
+  );
 
-    try {
-      const foundUser = await User.findOne({ username: username });
-      const compareResult = await bcrypt.compare(password, foundUser.password)
-
-      if (!foundUser) {
-        res.send("User not found!");
-      } else {
-        if (compareResult) {
-          console.log("User Logged Successfully");
-          res.redirect("/secrets");
-        } else {
-          res.send("password was wrong");
-        }
-      }
-    } catch (error) {
-      console.log("User failed to login!");
-      console.log(error);
-      res.send("Failed to Find user from database");
-    }
-  });
-
-// register route  
-app
-  .route("/register")
-  .get((req, res) => {
-    res.render("register");
-  })
-  .post(async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // for bcrypt password encryption
-    const saltRounds = 10
-    const hash  = await bcrypt.hash(password, saltRounds)
-
-    try {
-      const user = new User({
-        username: username,
-        password: hash,
-      });
-      await user.save();
-      console.log("User registered Successfully");
-      res.redirect("/secrets");
-    } catch (error) {
-      console.log("User failed to register!");
-      console.log(error);
-      res.redirect("register");
-    }
-  });
-
-// secret route  
-app.route("/secrets").get((req, res) => {
-  res.render("secrets");
+// secret route
+app.route("/secrets").get(async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).redirect("/login");
+  }
+  const foundSecrets = await Secret.find({})
+  res.render("secrets", {foundSecrets});
 });
 
 // submit secrets route
 app
   .route("/submit")
-  .get((req, res) => {
+  .get(async (req, res) => {
+    if (!req.isAuthenticated()) {
+      return res.status(401).redirect("/login");
+    }
     res.render("submit");
   })
   .post(async (req, res) => {
@@ -108,17 +157,19 @@ app
       console.log("secret message saved");
       res.redirect("/secrets");
     } catch (error) {
-      console.log("couldnt save secret message");
+      console.log("couldn/'t save secret message");
       console.log(error);
       res.redirect("/submit");
     }
   });
 
 // logout route
-app.route("/logout")
-.get((req, res)=>{
-    res.redirect("/")
-})
+app.route("/logout").get((req, res) => {
+  req.logout((err) => {
+    if (err) return res.status(500).send("Error logging out");
+    else return res.status(200).redirect("/");
+  });
+});
 
 // listening to port
 app.listen(PORT, () => {
